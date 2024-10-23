f4ab71f6bee86d1b58479b7415dcd6be258ec425
VaheM27 <vmnatsakanyan27@gmail.com>
2024-10-23 18:24:03 +0400

    Fixed navbar sticky

--- src/components/feature/Navbar/Navbar.jsx
+++ src/components/feature/Navbar/Navbar.jsx
@@ -1,15 +1,12 @@
-import icon from"../../../assets/images/Nav_images/logo.png";
-import searchlogo from"../../../assets/images/Nav_images/Search.png";
-import avatarlogo from"../../../assets/images/Nav_images/Avatar.png";
-import heartlogo from"../../../assets/images/Nav_images/Heart.png";
-import shopinglogo from"../../../assets/images/Nav_images/Shopping cart.png";
+import icon from "../../../assets/images/Nav_images/logo.png";
+import searchlogo from "../../../assets/images/Nav_images/Search.png";
+import avatarlogo from "../../../assets/images/Nav_images/Avatar.png";
+import heartlogo from "../../../assets/images/Nav_images/Heart.png";
+import shopinglogo from "../../../assets/images/Nav_images/Shopping cart.png";
 import { NavLink } from "react-router-dom";
 import { nanoid } from "nanoid";
 
-
-
 import "./Navbar.scss";
-
 
 export default function Navbar() {
   const images = [
@@ -29,62 +26,56 @@ export default function Navbar() {
       img: shopinglogo,
       id: nanoid(4),
     },
-  ]
+  ];
 
   const navItems = [
     {
-      name: 'Home',
+      name: "Home",
       id: nanoid(4),
-      path: '/'
+      path: "/",
     },
     {
-      name: 'Shop',
+      name: "Shop",
       id: nanoid(4),
-      path: 'shop'
+      path: "shop",
     },
     {
-      name: 'About',
+      name: "About",
       id: nanoid(4),
-      path: 'about'
+      path: "about",
     },
     {
-      name: 'Contact',
+      name: "Contact",
       id: nanoid(4),
-      path: 'contact'
+      path: "contact",
     },
     {
-      name: 'Cart',
+      name: "Cart",
       id: nanoid(4),
-      path: 'cart'
-    }
-  ]
-  return <div>
+      path: "cart",
+    },
+  ];
+  return (
     <nav>
       <div className="container">
-          <div className="nav_main">
-              <img src={icon} alt="" />
-              <ul>
-                {
-                  navItems.map((item)=>{
-                    return(
-                      <li key={item.id}><NavLink to={item.path}>{item.name}</NavLink></li>
-                    )
-                  })
-                }
-              </ul>
-              <div className="navImages">
-                {
-                  images.map((item)=>{
-                    return(
-                      <img src={item.img} alt="" key={item.id}/>
-                    )
-                  })
-                }
-              </div>
-          </div>      
+        <div className="nav_main">
+          <img src={icon} alt="" />
+          <ul>
+            {navItems.map((item) => {
+              return (
+                <li key={item.id}>
+                  <NavLink to={item.path}>{item.name}</NavLink>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="navImages">
+            {images.map((item) => {
+              return <img src={item.img} alt="" key={item.id} />;
+            })}
+          </div>
+        </div>
       </div>
     </nav>
-    
-
-  </div>;
+  );
 }
